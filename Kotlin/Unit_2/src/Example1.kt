75fe2ab993099fcbc4252527d4d25b77d69ce1bf
J Steven Perry <jstevenperry@gmail.com>
2018-09-01 18:57:58 -0500

    New Kotlin LP artifacts

---
+++ Kotlin/Unit_2/src/Example1.kt
@@ -0,0 +1,9 @@
+package com.makotojava.learn.kotlin
+
+fun printme(message: String) {
+    println(message)
+}
+
+fun sayit() {
+    printme("Hello, Kotlin!")
+}
---
+++ Kotlin/Unit_4/src/com/makotojava/learn/kotlin/Unit4.kt
@@ -0,0 +1,11 @@
+package com.makotojava.learn.kotlin
+
+class Unit4 {
+    fun badCompare() {
+        var a = 1;
+//        if (a = 1) { <-- This line won't compile!
+        if (a == 1) {
+
+        }
+    }
+}
