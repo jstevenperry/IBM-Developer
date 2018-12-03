36c98fa2abe13d1b63a92ac09a808db35820da77
J Steven Perry <jstevenperry@gmail.com>
2018-12-03 09:35:37 -0600

    Composer Queries: cleanup scripts

--- Hyperledger/Composer/Queries/secure-goods/scripts/createOrder.sh
+++ Hyperledger/Composer/Queries/secure-goods/scripts/createOrder.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# Usage message
+usage() {
+  echo "Usage: $0 [-aibsScvh]"
+  echo "Where:"
+  echo "-a ID_CARD    : the ID card to use for authentication (default: sell001@secure-goods)"
+  echo "-i ITEM_ID    : the Item ID to exchange (default: WIDGET001)"
+  echo "-b BUYER_ID   : the Buyer ID (default: buy001)"
+  echo "-s SELLER_ID  : the Seller ID (default: sell001)"
+  echo "-S SHIPPER_ID : the Shipper ID (default: ship001)"
+  echo "-c CURRENCY   : the Currency code to use (default: EUR)"
+  echo "-h            : this message"
+  echo ""
+  echo "Example: $0 -a sell002@secure-goods -i WIDGET002 -b buy001 -s sell002 -S ship001 -c AUD"
+  echo ""
+}
+
 # defaults
 auth=sell001@secure-goods
-widgetId=WIDGET001
+itemId=WIDGET001
 buyerId=buy001
 sellerId=sell001
 shipperId=ship001
@@ -10,45 +26,52 @@ currency=EUR
 
 # read the options
  
-while getopts "a:w:b:s:S:c:" opt; do
+while getopts "a:i:b:s:S:c:h" opt; do
   case $opt in
     a)
       auth=$OPTARG
-      echo "auth: $auth" >&2
       ;;
-    w)
-      widgetId=$OPTARG
-      echo "widgetId: $widgetId" >&2
+    i)
+      itemId=$OPTARG
       ;;
     b)
       buyerId=$OPTARG
-      echo "buyerId: $buyerId" >&2
       ;;
     s)
       sellerId=$OPTARG
-      echo "sellerId: $sellerId" >&2
       ;;
     S)
       shipperId=$OPTARG
-      echo "shipperId: : $shipperId" >&2
       ;;
     c)
       currency=$OPTARG
-      echo "currency: : $currency" >&2
+      ;;
+    h)
+      usage
+      exit 1
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      usage
       exit 1
       ;;
   esac
 done
+
+# Dump out the options
+echo "auth: $auth" >&2
+echo "itemId: $itemId" >&2
+echo "buyerId: $buyerId" >&2
+echo "sellerId: $sellerId" >&2
+echo "shipperId: : $shipperId" >&2
+echo "currency: : $currency" >&2
 
 #
 # Now execute the command using the parameters values supplied
 # or the defaults
 composer transaction submit --card ${auth} -d '{
  "$class": "com.makotogo.learn.composer.securegoods.asset.CreateOrder",
-  "item": "resource:com.makotogo.learn.composer.securegoods.asset.Item#'${widgetId}'",
+  "item": "resource:com.makotogo.learn.composer.securegoods.asset.Item#'${itemId}'",
   "quantity": 20,
   "unitCost": {
     "$class": "com.makotogo.learn.composer.securegoods.common.Price",
--- Hyperledger/Composer/Queries/secure-goods/scripts/receiveOrder.sh
+++ Hyperledger/Composer/Queries/secure-goods/scripts/receiveOrder.sh
@@ -1,19 +1,32 @@
 #! /bin/sh
 
+# Usage message
+usage() {
+  echo "Usage: $0 -o ORDER_ID [-ah]"
+  echo "Where:"
+  echo "-a ID_CARD  : the ID card to use for authentication (default: sell001@secure-goods)"
+  echo "-o ORDER_ID : the Order ID to ship (REQUIRED)"
+  echo "-h          : this message"
+  echo ""
+  echo "Example: $0 -a sell002@secure-goods -o ORD0000102"
+}
+
 # defaults
 auth=ship001@secure-goods
 
 # read the options
  
-while getopts "a:o:" opt; do
+while getopts "a:o:h" opt; do
   case $opt in
     a)
       auth=$OPTARG
-      echo "auth: $auth" >&2
       ;;
     o)
       orderId=$OPTARG
-      echo "OrderId: $orderId" >&2
+      ;;
+    h)
+      usage
+      exit 1
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -22,7 +35,19 @@ while getopts "a:o:" opt; do
   esac
 done
 
-composer transaction submit --card buy001@secure-goods -d '{
+# Dump out the options
+echo "auth: $auth" >&2
+echo "OrderId: $orderId" >&2
+
+# Complain, then exit, if no order ID is specified
+if [ -z $orderId ]; then
+  echo "No Order ID specified, cannot continue!" >&2
+  echo "" >&2
+  usage
+  exit 1
+fi
+
+composer transaction submit --card ${auth} -d '{
 "$class": "com.makotogo.learn.composer.securegoods.asset.ReceiveOrder",
 "order": "resource:com.makotogo.learn.composer.securegoods.asset.Order#'${orderId}'"
 }'
