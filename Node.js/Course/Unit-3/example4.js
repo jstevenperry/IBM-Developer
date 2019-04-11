5e4871a2a368472b81bfbe9878b3be67642e0302
J Steven Perry <jstevenperry@gmail.com>
2019-04-11 16:53:56 -0500

    Turns out, these were correct to begin with. Thanks, Nick.

--- Node.js/Course/Unit-3/example4.js
+++ Node.js/Course/Unit-3/example4.js
@@ -17,9 +17,12 @@
 var fs = require('fs');
 console.log('Starting program...');
 // Read a small file asynchronously
-var fileContents = fs.readFileSync('../data/50Words.txt', 'utf8');
-// Count number of words
-var numberOfWords = fileContents.split(/[ ,.\n]+/).length;
-// Print to console
-console.log('There are ' + numberOfWords + ' words in this file.');
-console.log('Program finished.');
+fs.readFile('../data/50Words.txt', 'utf8', function(err, fileContents) {
+    // If error occurred, rethrow error
+    if (err) throw err;
+    // Count number of words
+    let numberOfWords = fileContents.split(/[ ,.\n]+/).length;
+    // Print to console
+    console.log('There are ' + numberOfWords + ' words in this file.');
+});
+console.log('Program finished');
