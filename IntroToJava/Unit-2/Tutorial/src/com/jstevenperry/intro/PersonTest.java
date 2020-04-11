861ffc504d36923bc46cb85165d4e6d718dd38a9
J Steven Perry <jstevenperry@gmail.com>
2020-04-10 19:46:25 -0500

    Add Logger to PersonTest

--- IntroToJava/Unit-2/Tutorial/src/com/jstevenperry/intro/PersonTest.java
+++ IntroToJava/Unit-2/Tutorial/src/com/jstevenperry/intro/PersonTest.java
@@ -2,18 +2,23 @@ package com.jstevenperry.intro;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.Test;
 
 class PersonTest {
 
 	@Test
 	public void testPerson() {
-		Person p = new Person("Joe Q Author", 52, 174, 80, "Brown", "MALE");
+		Person p = new Person("Joe Q Author", 42, 173, 82, "Brown", "MALE");
+		
+		Logger l = Logger.getLogger(Person.class.getName());
+		l.info("Created Person object named: " + p.getName());
 		
 		assertEquals("Joe Q Author", p.getName());
-		assertEquals(52, p.getAge());
-		assertEquals(174, p.getHeight());
-		assertEquals(80, p.getWeight());
+		assertEquals(42, p.getAge());
+		assertEquals(173, p.getHeight());
+		assertEquals(82, p.getWeight());
 		assertEquals("Brown", p.getEyeColor());
 		assertEquals("MALE", p.getGender());
 	}
